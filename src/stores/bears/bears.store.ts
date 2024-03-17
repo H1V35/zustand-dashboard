049b3cd14351a9d3023234fd35c6e7a8df3542dd
H1V35 <higesjorge@gmail.com>
2024-03-17 17:57:08 +0100

    ✨ feat: Use 'bears.store' in BearPage

--- src/stores/bears/bears.store.ts
+++ src/stores/bears/bears.store.ts
@@ -6,6 +6,8 @@ interface BearState {
   pandaBears: number;
 
   increaseBlackBears: (by: number) => void;
+  increasePolarBears: (by: number) => void;
+  increasePandaBears: (by: number) => void;
 }
 
 export const useBearStore = create<BearState>()((set) => ({
@@ -14,4 +16,6 @@ export const useBearStore = create<BearState>()((set) => ({
   pandaBears: 1,
 
   increaseBlackBears: (by: number) => set((state) => ({ blackBears: state.blackBears + by })),
+  increasePolarBears: (by: number) => set((state) => ({ polarBears: state.polarBears + by })),
+  increasePandaBears: (by: number) => set((state) => ({ pandaBears: state.pandaBears + by })),
 }));
