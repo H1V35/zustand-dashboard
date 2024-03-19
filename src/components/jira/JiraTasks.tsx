bc079d5ce236181668b82d9c36139388e96ac141
H1V35 <higesjorge@gmail.com>
2024-03-19 17:49:49 +0100

    ✨ feat: Add 'sweetalert2' library for quick task name input; Refactor Props interface in JiraTasks component

--- src/components/jira/JiraTasks.tsx
+++ src/components/jira/JiraTasks.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { IoAddOutline, IoCheckmarkCircleOutline } from 'react-icons/io5';
 import classNames from 'classnames';
+import Swal from 'sweetalert2';
 import { SingleTask } from './SingleTask';
 import { useTasksStore } from '@/stores';
 import type { Task, TaskStatus } from '@/interfaces';
@@ -8,18 +9,30 @@ import type { Task, TaskStatus } from '@/interfaces';
 interface Props {
   title: string;
   tasks: Task[];
-  value: TaskStatus;
+  status: TaskStatus;
 }
 
-export const JiraTasks = ({ title, tasks, value }: Props) => {
+export const JiraTasks = ({ title, tasks, status }: Props) => {
   const isDragging = useTasksStore((state) => !!state.draggingTaskId);
   const addTask = useTasksStore((state) => state.addTask);
   const onTaskDrop = useTasksStore((state) => state.onTaskDrop);
 
   const [onDragOver, setOnDragOver] = React.useState(false);
 
-  const handleAddTask = () => {
-    addTask('New Title', value);
+  const handleAddTask = async () => {
+    const { isConfirmed, value } = await Swal.fire({
+      title: 'Nueva tarea',
+      input: 'text',
+      inputLabel: 'Nombre de la tarea',
+      inputPlaceholder: 'Ingrese el nombre de la tarea',
+      showCancelButton: true,
+      inputValidator: (value) => {
+        if (!value) return 'Debe ingresar un nombre para la tarea';
+      },
+    });
+
+    if (!isConfirmed) return;
+    addTask(value, status);
   };
 
   const handleDragOver = (event: React.DragEvent<HTMLDivElement>) => {
@@ -35,7 +48,7 @@ export const JiraTasks = ({ title, tasks, value }: Props) => {
   const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
     event.preventDefault();
     setOnDragOver(false);
-    onTaskDrop(value);
+    onTaskDrop(status);
   };
 
   return (
