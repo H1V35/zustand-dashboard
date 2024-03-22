71c19bfd173cf5ad79572c0bbbd1cc242e8705b9
H1V35 <higesjorge@gmail.com>
2024-03-22 16:27:08 +0100

    🔑 feat: Implement authentication state check; Add necessary interceptor for authorization with bearer token

--- src/api/tesloApi.ts
+++ src/api/tesloApi.ts
@@ -1,8 +1,16 @@
 import axios from 'axios';
+import { useAuthStore } from '@/stores';
 
 export const tesloApi = axios.create({
   baseURL: 'http://localhost:3000/api',
 });
 
-// TODO: Interceptors
-// Read Zustand store
+tesloApi.interceptors.request.use((config) => {
+  const token = useAuthStore.getState().token;
+
+  if (token) {
+    config.headers['Authorization'] = `Bearer ${token}`;
+  }
+
+  return config;
+});
